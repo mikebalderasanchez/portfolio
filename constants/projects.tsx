dded993a59ab83e3b66dde070ce0a1aa14439d48
mikebalderasanchez <mikebalderassanchez@gmail.com>
2025-11-27 18:28:31 -0600

    fix: projects

--- constants/projects.tsx
+++ constants/projects.tsx
@@ -20,24 +20,24 @@ export const projects: {
     color: "bg-[#000080]",
     techs: [<ReactLogo key="react" />, <Expo key="expo" />, <TypeScript key="typescript" />, <GoogleCloud key="google-cloud" />],
   },
-  {
-    id: "aaf",
-    quote:
-      "Always and Forever is an app designed to preserve your most cherished memories as a couple. It’s the perfect digital space to collect and relive your shared journey, from the smallest moments to the biggest milestones",
-    name: "Always and Forever",
-    designation: "The storehouse of your love story",
-    src: "/projects/aaf.png",
-    color: "bg-[#ff0000]",
-    techs: [<ReactLogo key="react" />, <Expo key="expo" />, <TypeScript key="typescript" />, <GoogleCloud key="google-cloud" />],
-  },
-  {
-    id: "finance",
-    quote:
-      "Keep track of your income, expenses, and card payments in one place. View your due dates, schedule payments, and avoid debt without complications.",
-    name: "Finance",
-    designation: "The simple way to manage your money",
-    src: "/projects/finance.png",
-    color: "bg-[#000090]",
-    techs: [<ReactLogo key="react" />, <Expo key="expo" />, <TypeScript key="typescript" />, <GoogleCloud key="google-cloud" />],
-  },
+  // {
+  //   id: "aaf",
+  //   quote:
+  //     "Always and Forever is an app designed to preserve your most cherished memories as a couple. It’s the perfect digital space to collect and relive your shared journey, from the smallest moments to the biggest milestones",
+  //   name: "Always and Forever",
+  //   designation: "The storehouse of your love story",
+  //   src: "/projects/aaf.png",
+  //   color: "bg-[#ff0000]",
+  //   techs: [<ReactLogo key="react" />, <Expo key="expo" />, <TypeScript key="typescript" />, <GoogleCloud key="google-cloud" />],
+  // },
+  // {
+  //   id: "finance",
+  //   quote:
+  //     "Keep track of your income, expenses, and card payments in one place. View your due dates, schedule payments, and avoid debt without complications.",
+  //   name: "Finance",
+  //   designation: "The simple way to manage your money",
+  //   src: "/projects/finance.png",
+  //   color: "bg-[#000090]",
+  //   techs: [<ReactLogo key="react" />, <Expo key="expo" />, <TypeScript key="typescript" />, <GoogleCloud key="google-cloud" />],
+  // },
 ];
